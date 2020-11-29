732b680edcdb259f185c7f25a954fca6a8c6f87f
Aayushgarg71 <aayushgarg71@gmail.com>
2020-11-29 10:22:29 +0530

    Entity made for Room db

---
+++ app/src/main/java/com/example/notes/Note.kt
@@ -0,0 +1,12 @@
+package com.example.notes
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName="noteTable")
+class Note(@ColumnInfo (name="text")val text:String) {
+    @PrimaryKey(autoGenerate = true)val id= Int
+
+}
---
+++ app/src/main/java/com/example/notes/NoteDao.kt
@@ -0,0 +1,8 @@
+package com.example.notes
+
+import androidx.room.Dao
+
+@Dao
+interface NoteDao {
+
+}
