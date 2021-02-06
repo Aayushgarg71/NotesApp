e4b15395503eb93ea1b084f0694abcc453dc8516
Aayushgarg71 <aayushgarg71@gmail.com>
2021-02-06 14:42:16 +0530

    Made recycler view for the app.

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -4,15 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), INotesRVAdapter {
     lateinit var viewModel: NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recycler.layoutManager=LinearLayoutManager(this)
+        val adapter=NotesRVAdapter(this,this)
+        recycler.adapter=adapter
         viewModel= ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
-        viewModel.allNotes.observe(this, Observer{
+        viewModel.allNotes.observe(this, Observer{ list->
+            list?.let{
+                adapter.updateList(it)
+            }
 
         })
+
+    }
+
+    override fun onItemClicked(note: Note) {
+
     }
 }
